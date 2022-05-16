4a2303b758c1655bf94d4ca5eef999fe3fba937d
yosi benziman <yosibenziman@yosis-MacBook-Pro.local>
2022-05-16 14:22:52 +0300

    add master_authorized_networks_config to composer_1

--- modules/create_environment_v1/main.tf
+++ modules/create_environment_v1/main.tf
@@ -106,5 +106,19 @@ resource "google_composer_environment" "composer_env" {
         kms_key_name = encryption_config.value["kms_key_name"]
       }
     }
+
+    dynamic "master_authorized_networks_config" {
+      for_each = local.master_authorized_networks_config
+      content {
+        enabled = length(var.master_authorized_networks) > 0
+        dynamic "cidr_blocks" {
+          for_each = master_authorized_networks_config.value["cidr_blocks"]
+          content {
+            cidr_block   = master_authorized_networks_config.value["cidr_block"]
+            display_name = master_authorized_networks_config.value["display_name"]
+          }
+        }
+      }
+    }
   }
 }
