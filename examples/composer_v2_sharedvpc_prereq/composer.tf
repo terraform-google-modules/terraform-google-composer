5302ec00abc4bb48f4bcb65192175797afbedcb1
Sanmay <sanmaym@google.com>
2022-10-27 15:56:55 -0400

    moved out of fixtures

--- examples/composer_v2_sharedvpc_prereq/composer.tf
+++ examples/composer_v2_sharedvpc_prereq/composer.tf
@@ -11,39 +11,58 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+module "simple-shared-vpc-composer" {
+  source = "../../../examples/composer_v2_sharedvpc_prereq"
+
+  service_project_id                     = "ctl-new-svc"
+  network_project_id                     = "ctl-new-hvpc"
+  composer_env_name                      = "san-composer-2"
+  region                                 = "us-central1"
+  network                                = "composer-network"
+  subnetwork                             = "composer-subnetwork"
+  cloud_composer_network_ipv4_cidr_block = "192.168.192.0/24"
+  master_ipv4_cidr                       = "192.168.193.0/28"
+  cloud_sql_ipv4_cidr                    = "192.168.0.0/17"
+  pod_ip_allocation_range_name           = "composer-pods-1"
+  service_ip_allocation_range_name       = "composer-services-1"
+  gke_subnet_ip_range                    = ["10.100.232.0/27"]
+  gke_pods_services_ip_ranges            = ["10.1.0.0/16", "10.4.0.0/16", "10.10.10.0/24", "10.10.14.0/24"]
+}
 
 module "composer_net" {
   source                                 = "../../modules/composer_net"
-  service_project_id                     = var.service_project_id
-  network_project_id                     = var.network_project_id
-  region                                 = var.region
-  network                                = var.network
-  subnetwork                             = var.subnetwork
-  composer_env_name                      = var.composer_env_name
-  gke_pods_services_ip_ranges            = var.gke_pods_services_ip_ranges
-  gke_subnet_ip_range                    = var.gke_subnet_ip_range
-  cloud_composer_network_ipv4_cidr_block = var.cloud_composer_network_ipv4_cidr_block
-  master_ipv4_cidr                       = var.master_ipv4_cidr
-  cloud_sql_ipv4_cidr                    = var.cloud_sql_ipv4_cidr
+  service_project_id                     = "ctl-new-svc"
+  network_project_id                     = "ctl-new-hvpc"
+  composer_env_name                      = "san-composer-2"
+  region                                 = "us-central1"
+  network                                = "composer-network"
+  subnetwork                             = "composer-subnetwork"
+  cloud_composer_network_ipv4_cidr_block = "192.168.192.0/24"
+  master_ipv4_cidr                       = "192.168.193.0/28"
+  cloud_sql_ipv4_cidr                    = "192.168.0.0/17"
+  pod_ip_allocation_range_name           = "composer-pods-1"
+  service_ip_allocation_range_name       = "composer-services-1"
+  gke_subnet_ip_range                    = ["10.100.232.0/27"]
+  gke_pods_services_ip_ranges            = ["10.1.0.0/16", "10.4.0.0/16", "10.10.10.0/24", "10.10.14.0/24"]
 }
 module "composer_env" {
   depends_on = [
     module.composer_net
   ]
   source = "../../modules/create_environment_v2"
-  # source                                 = "terraform-google-modules/composer/google//modules/create_environment_v2"
-  project_id                             = var.service_project_id
-  network_project_id                     = var.network_project_id
-  composer_env_name                      = var.composer_env_name
-  region                                 = var.region
-  composer_service_account               = module.composer_net.composer_sa_email
-  network                                = var.network
-  subnetwork                             = var.subnetwork
-  pod_ip_allocation_range_name           = var.pod_ip_allocation_range_name
-  service_ip_allocation_range_name       = var.service_ip_allocation_range_name
-  cloud_composer_network_ipv4_cidr_block = var.cloud_composer_network_ipv4_cidr_block
-  master_ipv4_cidr                       = var.master_ipv4_cidr
-  cloud_sql_ipv4_cidr                    = var.cloud_sql_ipv4_cidr
+  service_project_id                     = "ctl-new-svc"
+  network_project_id                     = "ctl-new-hvpc"
+  composer_env_name                      = "san-composer-2"
+  region                                 = "us-central1"
+  network                                = "composer-network"
+  subnetwork                             = "composer-subnetwork"
+  cloud_composer_network_ipv4_cidr_block = "192.168.192.0/24"
+  master_ipv4_cidr                       = "192.168.193.0/28"
+  cloud_sql_ipv4_cidr                    = "192.168.0.0/17"
+  pod_ip_allocation_range_name           = "composer-pods-1"
+  service_ip_allocation_range_name       = "composer-services-1"
+  gke_subnet_ip_range                    = ["10.100.232.0/27"]
+  gke_pods_services_ip_ranges            = ["10.1.0.0/16", "10.4.0.0/16", "10.10.10.0/24", "10.10.14.0/24"]
   grant_sa_agent_permission              = true
   use_private_environment                = true
   enable_private_endpoint                = true
