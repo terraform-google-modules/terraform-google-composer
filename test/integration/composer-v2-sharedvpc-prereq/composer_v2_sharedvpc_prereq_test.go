561348d90b63b9107c24ff05d8bfe8136054f5ff
Sanmay <sanmaym@google.com>
2022-10-27 16:05:33 -0400

    changed package name

--- test/integration/composer-v2-sharedvpc-prereq/composer_v2_sharedvpc_prereq_test.go
+++ test/integration/composer-v2-sharedvpc-prereq/composer_v2_sharedvpc_prereq_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package composer-v2-sharedvpc-prereq
+package composer_v2_sharedvpc_prereq
 
 import (
 	"fmt"
