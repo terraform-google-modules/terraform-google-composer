0d66cf9aa2570fb7c893758e23846ea12517234c
Nicolas Levée <nlevee@users.noreply.github.com>
2024-02-22 15:44:52 +0100

    feat(dns): add suffix to composer dns zone
    In case of multiple composer instance

--- modules/composer_net/dns.tf
+++ modules/composer_net/dns.tf
@@ -16,8 +16,13 @@
 composer.cloud.google.com
 ***************************************/
 
+resource "random_string" "composer_cloud_zone" {
+  length           = 4
+  special          = false
+}
+
 resource "google_dns_managed_zone" "composer_cloud_zone" {
-  name        = "composer-google-cloud-dns"
+  name        = "composer-google-cloud-dns-${random_string.composer_cloud_zone.id}"
   project     = var.network_project_id
   dns_name    = "composer.cloud.google.com."
   description = "composer.cloud.google.com zone"
