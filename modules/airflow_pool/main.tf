c906fc503ff975f32af1c7690a2cc60c13afdd3d
Vincent Göckeritz <vincent.goeckeritz@og1o.de>
2025-10-09 13:57:41 +0200

    Update gcloud command syntax and version

--- modules/airflow_pool/main.tf
+++ modules/airflow_pool/main.tf
@@ -15,14 +15,14 @@
  */
 
 locals {
-  gcloud_cmd_body  = "composer environments run --project=${var.project_id} --location=${var.region} ${var.composer_env_name} pool"
-  create_cmd_body  = "${local.gcloud_cmd_body} -- --set ${jsonencode(var.pool_name)} ${jsonencode(var.slot_count)} ${jsonencode(var.description)}"
-  destroy_cmd_body = "${local.gcloud_cmd_body} -- --delete ${jsonencode(var.pool_name)}"
+  gcloud_cmd_body  = "composer environments run --project=${var.project} --location=${var.region} ${var.composer_env_name} pools"
+  create_cmd_body  = "${local.gcloud_cmd_body} -- set ${jsonencode(var.pool_name)} ${jsonencode(var.slot_count)} ${jsonencode(var.description)}"
+  destroy_cmd_body = "${local.gcloud_cmd_body} -- delete ${jsonencode(var.pool_name)}"
 }
 
 module "gcloud" {
   source           = "terraform-google-modules/gcloud/google"
-  version          = "~> 3.1"
+  version          = "~> 4.0"
   platform         = "linux"
   create_cmd_body  = local.create_cmd_body
   destroy_cmd_body = local.destroy_cmd_body
